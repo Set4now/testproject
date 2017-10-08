6e63f2ee64db53cec508411799eb5124dc35555c
Set4now <sumandeb_git@yahoo.co.in>
2017-10-08 19:54:24 +0530

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #this will start the salt services and all other dependencies services for this container
 LOG_LEVEL=${LOG_LEVEL:-"info"}
-/etc/init.d/sshd start
-/etc/init.d/salt-minion start
+/usr/bin/ssh-keygen -A
+/usr/sbin/sshd -D
+/usr/bin/salt-minion
 sleep 20
 salt-call --local tls.create_self_signed_cert
-/etc/init.d/salt-api start
+#/etc/init.d/salt-api start
 # Run Salt as a Deamon
 /usr/bin/salt-master --log-level=$LOG_LEVEL
