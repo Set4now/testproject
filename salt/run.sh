2796abb1a540fd2cead6721a8a6a5697be65994d
Set4now <sumandeb_git@yahoo.co.in>
2017-10-09 01:56:40 +0530

    Update run.sh

--- salt/run.sh
+++ salt/run.sh
@@ -3,9 +3,31 @@
 LOG_LEVEL=${LOG_LEVEL:-"info"}
 /usr/bin/ssh-keygen -A
 /usr/sbin/sshd -D
+if [ $? -ne 0];then
+echo "sshd service failed"
+exit 1
+fi
 /usr/bin/salt-minion -d
+if [ $? -ne 0];then
+echo "salt minion service failed"
+exit 1
+fi
 sleep 20
 salt-call --local tls.create_self_signed_cert
 #/etc/init.d/salt-api start
 # Run Salt as a Deamon
 /usr/bin/salt-master -d --log-level=$LOG_LEVEL
+if [ $? -ne 0];then
+echo "salt master service failed"
+exit 1
+fi
+ps -ef | grep -i salt-master | grep -v grep
+exit_status_salt-master=$?
+ps -ef | grep -i salt-minion | grep -v grep
+exit_status_salt-minion=$?
+if [ exit_status_salt-master -ne 0 -o exit_status_salt-minion -ne 0 ];then
+echo "One of the process exited"
+exit -1
+fi
+
+
